44ef14611d71c9deacc7c4ca99ce75da0eb5819f
PeuStone <pedrohmotaaa@gmail.com>
2024-03-06 09:43:26 -0300

    Criando estrutura basica de Design System e resolvendo problema de importação de modulos com o next transpile modules e next compose plugins

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { sum } from "@alura/utils/math/sum"
+import React from 'react';
+import { sum } from "@alura/utils/math/sum";
+import { Text } from "@alura/design-system/components/Text";
 
 export default function HomeScreen() {
-    return (
-        <main>
-            <h1>Home</h1>
-            <p>Importando modulo local: @alura/utils/math/sum sum(2,2): {sum(2,2)}</p>
-        </main>
-    )
+  return (
+    <main>
+      <Text tag='h1'>Home</Text >
+      <Text tag='p'>Importando modulo local: @alura/utils/math/sum sum(2,2): {sum(2, 2)}</Text>
+    </main>
+  )
 }
