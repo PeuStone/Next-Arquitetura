976c65634a7fffdde8648668e581c29bd13ea3de
PeuStone <pedrohmotaaa@gmail.com>
2024-03-07 11:14:17 -0300

    Reutilizando o setup criado para uso do Jest para testes

---
+++ packages/utils/math/sum.test.ts
@@ -0,0 +1,11 @@
+import { describe } from "node:test";
+import { sum } from "./sum";
+
+describe("sum()", () => {
+  it("when receives 1 and 1, return 2", () => {
+    expect(sum(1, 1)).toBe(2);
+  });
+  it("when receives 2 and 2, return 4", () => {
+    expect(sum(1, 1)).toBe(2);
+  });
+});
