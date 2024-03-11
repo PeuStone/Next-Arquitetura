bff05a7240c41434f11d58a23df1107ba6d4355c
PeuStone <pedrohmotaaa@gmail.com>
2024-03-11 10:12:33 -0300

    Adicionando o Jest-Styled-Components e mais testes no projeto

---
+++ projects/web-public/src/screens/HomeScreen/index.test.tsx
@@ -0,0 +1,11 @@
+import { HomeScreen } from ".";
+import { customRender } from "@alura/test-commons/react-testing-library";
+
+const render = customRender();
+
+describe("<HomeScreen />", () => {
+  it("renders the page", () => {
+    const { container } = render(<HomeScreen />);
+    expect(container).toMatchSnapshot();
+  });
+});
